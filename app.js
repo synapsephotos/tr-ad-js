93c01ac9f761eda12787ae0a65b8962c7d18f219
synapsephotos <135157942+synapsephotos@users.noreply.github.com>
2024-10-29 13:50:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const chalk = require("chalk");
 const server = express();
+
 server.all("/", (req, res) => res.send(`<meta http-equiv="refresh" content="0; URL=https://passwordpassword.online"/>`));
 server.listen(process.env.PORT ?? 3000, () => {
-    console.log(`${chalk.cyanBright.bold("clan-advertisements")} | ${chalk.greenBright.bold("The Revengeance")}`);
-
-  console.log(`\n[${chalk.green.bold("+")}] The webserver is ready.\n`);
+    console.log(`${chalk.magentaBright.bold("clan-advertisements")} | ${chalk.redBright.bold("The Revengeance")}`);
+    console.log(`\n[${chalk.green.bold("+")}] The webserver is ready.\n`);
 });
 
-const Authorization = process.env.Authorization
+const Authorization = process.env.Authorization;
+
 const CHANNEL_ID = "1245731705113940089"
 const MESSAGE = "LF people that can upgrade 10 times a day and afk (we need one more level for quantum) 7 spots available\n> **DM <@1016542844170817568> to apply**"
 
@@ -21,9 +22,8 @@ client.once('ready', async () => {
   setInterval(async () =>{
     try {
       const channel = await client.channels.fetch(CHANNEL_ID);
-      
-      channel.send(MESSAGE);
-      console.log(`[${chalk.green.bold("+")}] Message sent successfully.`);
+      await channel.send(MESSAGE);
+      console.log(`[${chalk.green.bold("+")}] Message sent successfully`);
     } catch (error) {
       console.error('Error sending message:', error.message);
     }
